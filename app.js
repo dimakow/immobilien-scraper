1d32be0fc28cec724f617f80001c77bf8b1ec7e8
dimakow <dimakow@nextstack.de>
2017-05-07 16:09:25 -0700

    introducing different modes in which handler can be called

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ let dynamodb = new AWS.DynamoDB();
 
 let content;
 let tableName = "adindex";
-let urlPart1 = 'https://www.immobilienscout24.de/Suche/S-T'
-let urlPart2 = '/Wohnung-Miete/Berlin/Berlin/Wedding-Wedding/2,00-/-/EURO--700,00?enteredFrom=one_step_search'
+let urlPart1 = 'https://www.immobilienscout24.de/'
+let urlPart2 = 'Suche/S-T/Wohnung-Miete/Berlin/Berlin/Wedding-Wedding/2,00-/-/EURO--700,00?enteredFrom=one_step_search'
 
 let urlHelper = require('./lib/urlHelper')
 const fetchUrlBody = urlHelper.fetchUrlBody
@@ -24,21 +24,42 @@ let fnSaveItemToDB = function(item) {
     });
 }
 
+let fnCheckBodyAndSaveItems = function(body) {
+    let $ = cheerio.load(body)
+
+    $('.result-list__listing').each(function(i, elem) {
+        let ad = {
+            'data-id': {
+                N: $(this).attr('data-id')
+            }
+
+        }
+        fnSaveItemToDB(ad);
+    })
+}
+
 exports.myHandler = function(event, context) {
 
-    fetchUrlBody(urlPart1 + urlPart2).then(function(res) {
+    switch (event.mode) {
+        case "grabAll":
+            fetchUrlBody(urlPart1 + urlPart2).then(function(res) {
 
-        let $ = cheerio.load(res)
+                let $ = cheerio.load(res)
 
-        $('.result-list__listing').each(function(i, elem) {
-            let ad = {
-                'data-id': {
-                    N: $(this).attr('data-id')
-                }
+                $('#pageSelection > .select').children().each(function(i, elem) {
 
-            }
-            fnSaveItemToDB(ad);
-        })
-    })
+                    urlPart2 = $(this).attr('value')
 
+                    console.log($(this).html())
+
+                    urlPart2 = urlPart2.substring(1, urlPart2.length)
+
+                    fetchUrlBody(urlPart1 + urlPart2).then(res => {
+                        fnCheckBodyAndSaveItems(res)
+
+                    })
+                })
+            })
+        case "grabAllDetails":
+    }
 }
